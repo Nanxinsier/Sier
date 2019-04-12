b3d60ab69cd68ba9ec32e2205e3a10f5f6f6ef76
Nanxinsier <43698315+Nanxinsier@users.noreply.github.com>
2019-04-12 09:58:36 +0800

    Add files via upload

---
+++ class10.2.js
@@ -0,0 +1,6 @@
+function init() {
+  document.getElementById('agou')
+  divTag.innerHTML = ''
+  divTag.setAttribute('style','height: 52px; width: 70px; background-color: green; margin-left: 111px; margin-top: 90px;')
+  windowonload = init
+}
