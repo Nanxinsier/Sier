14d172c060bf70c870ee51dbb70131bfe51d9b2c
Nanxinsier <43698315+Nanxinsier@users.noreply.github.com>
2019-04-16 11:35:50 +0800

    Add files via upload

---
+++ class11.js
@@ -0,0 +1,13 @@
+console.log (WhatEver);
+var WhatEver = document.getElementById('amao');
+console.log (WhatEver);
+console.log (1/0);
+console.log (0/100);
+console.log ('aa' * 1);
+
+if (undefined === null)
+if (100 === '100')
+if ('1' === true)
+
+typeof (100)
+typeof ('100')
